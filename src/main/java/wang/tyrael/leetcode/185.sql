ca650bcf763d86e3976c47f80d88d184279d28fd
王超 <343331640@qq.com>
2018-01-09 10:06:48 +0800

    top3

--- src/main/java/wang/tyrael/leetcode/185.sql
+++ src/main/java/wang/tyrael/leetcode/185.sql
@@ -1,13 +1,13 @@
-create table Employee{
+create table Employee(
     Id int,
     Name varchar(20),
     Salary int,
     DepartmentId int
-};
-create table Name{
+);
+create table Department(
     Id int,
     Name varchar(20)
-};
+);
 insert into Employee values (1, 'Joe', 70000 ,1);
 insert into Employee values (2, 'Henry', 80000   ,2);
 insert into Employee values (3, 'Sam   ', 60000   ,2);
@@ -18,12 +18,23 @@ insert into Employee values (6, 'Randy ', 85000   ,1);
 insert into Department values (1, 'IT');
 insert into Department values (2, 'Sales');
 
-select Department.Name as Department, Employee.Name as Employee, Salary
-from Employee, Department
-where Employee.id in(
-    select Employee.Id
-    from Employee
-    where DepartmentId in(select Id from Department)
-    orderBy Salary
-    limit 3,1
+# 直接的思路是子查询、in、limit
+# mysql不支持，则转化为count，模拟实现limit
+SELECT
+	d. NAME AS Department,
+	e1. NAME AS Employee,
+	Salary
+FROM
+	Employee as e1
+INNER JOIN Department AS d ON e1.DepartmentId = d.Id
+where 3 >=
+(
+	SELECT count(DISTINCT
+		e2.Salary
+	)
+	FROM
+		Employee e2
+	WHERE e2.Salary >= e1.Salary and e2.DepartmentId = e1.DepartmentId
 )
+ORDER BY Department, Salary desc
+
