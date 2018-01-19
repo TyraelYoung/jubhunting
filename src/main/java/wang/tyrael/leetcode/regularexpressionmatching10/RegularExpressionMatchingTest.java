63eff796ad1daef550282b2fb68ac5b7f8579dc6
王超 <343331640@qq.com>
2018-01-19 18:18:45 +0800

    正则

--- src/main/java/wang/tyrael/leetcode/regularexpressionmatching10/RegularExpressionMatchingTest.java
+++ src/main/java/wang/tyrael/leetcode/regularexpressionmatching10/RegularExpressionMatchingTest.java
@@ -5,53 +5,66 @@ public class RegularExpressionMatchingTest {
         Solution solution = new Solution();
         boolean result;
 
+//        "acaabbaccbbacaabbbb"
+//        "a*.*b*.*a*aa*a*"
+        result = solution.isMatch("acaabbaccbbacaabbbb", "a*.*b*.*a*aa*a*");
+        System.out.println(" true:" + result);
+
+        result = solution.isMatch("", "a*");
+        System.out.println(" true:" + result);
+
+        result = solution.isMatch("a", "a*");
+        System.out.println(" true:" + result);
+
 //        "abbabaaaaaaacaa"
 //        "a*.*b.a.*c*b*a*c*"
         result = solution.isMatch("abbabaaaaaaacaa", "a*.*b.a.*c*b*a*c*");
         System.out.println(" true:" + result);
 //        ""
 //        ".ac*"
-//        result = solution.isMatch("", ".ac*");
-//        System.out.println(" false:" + result);
-//
-////        ""
-////        "c*c*"
-//        result = solution.isMatch("", "c*c*");
-//        System.out.println(" true:" + result);
-//
-//        result = solution.isMatch("aa", "a");
-//        System.out.println(" false:" + result);
-//        result = solution.isMatch("aa", "aa");
-//        System.out.println(" true:" + result);
-//        result = solution.isMatch("aaa", "aa");
-//        System.out.println(" false:" + result);
-//        result = solution.isMatch("aa", "a*");
-//        System.out.println(" true:" + result);
-//        result = solution.isMatch("aa", ".*");
-//        System.out.println(" true:" + result);
-//        result = solution.isMatch("ab", ".*");
-//        System.out.println(" true:" + result);
-//        result = solution.isMatch("aab", "c*a*b");
-//        System.out.println(" true:" + result);
-//
-////        "a"
-////        ".*..a*"
-//        result = solution.isMatch("a", ".*..a*");
-//        System.out.println(" false:" + result);
-////        "a"
-////        ""
-//        result = solution.isMatch("a", "");
-//        System.out.println(" false:" + result);
-//
-////        ""
-////        "bab"
-//        result = solution.isMatch("", "bab");
-//        System.out.println(" false:" + result);
-//
-////        "ab"
-////        ".*..c*"
-//
-//        result = solution.isMatch("ab", ".*..c*");
-//        System.out.println(" true:" + result);
+        result = solution.isMatch("", ".ac*");
+        System.out.println(" false:" + result);
+
+//        ""
+//        "c*c*"
+        result = solution.isMatch("", "c*c*");
+        System.out.println(" true:" + result);
+
+        result = solution.isMatch("aa", "a");
+        System.out.println(" false:" + result);
+        result = solution.isMatch("aa", "aa");
+        System.out.println(" true:" + result);
+        result = solution.isMatch("aaa", "aa");
+        System.out.println(" false:" + result);
+
+        result = solution.isMatch("aa", "a*");
+        System.out.println(" true:" + result);
+
+        result = solution.isMatch("aa", ".*");
+        System.out.println(" true:" + result);
+        result = solution.isMatch("ab", ".*");
+        System.out.println(" true:" + result);
+        result = solution.isMatch("aab", "c*a*b");
+        System.out.println(" true:" + result);
+
+//        "a"
+//        ".*..a*"
+        result = solution.isMatch("a", ".*..a*");
+        System.out.println(" false:" + result);
+//        "a"
+//        ""
+        result = solution.isMatch("a", "");
+        System.out.println(" false:" + result);
+
+//        ""
+//        "bab"
+        result = solution.isMatch("", "bab");
+        System.out.println(" false:" + result);
+
+//        "ab"
+//        ".*..c*"
+
+        result = solution.isMatch("ab", ".*..c*");
+        System.out.println(" true:" + result);
     }
 }
--- src/main/java/wang/tyrael/leetcode/regularexpressionmatching10/Solution.java
+++ src/main/java/wang/tyrael/leetcode/regularexpressionmatching10/Solution.java
@@ -3,67 +3,22 @@ package wang.tyrael.leetcode.regularexpressionmatching10;
 public class Solution {
     public boolean isMatch(String aa, String a) {
         boolean result;
+        returnResult:
         switch (a.length()) {
             case 0:
-                result =  aa.length() == 0;
-                break;
+                result = aa.length() == 0;
+                break returnResult;
             case 1:
                 if (a.equals(".")) {
                     result = aa.length() == 1;
                 } else {
                     result = aa.equals(a);
                 }
-                break;
+                break returnResult;
             case 2:
+            default:
                 char zeroA = a.charAt(0);
                 char oneA = a.charAt(1);
-                if (oneA == '*') {
-                    switch (aa.length()) {
-                        case 0:
-                            result = true;
-                            break;
-                        case 1:
-                            if (zeroA == '.') {
-                                result = true;
-                            } else {
-                                char zeroAA = aa.charAt(0);
-                                if (zeroA == zeroAA) {
-                                    result = true;
-                                } else {
-                                    result = false;
-                                }
-                            }
-                            break;
-                        case 2:
-                        default:
-                            if (zeroA == '.') {
-                                result = true;
-                            } else {
-                                 result = isOneChar(aa, zeroA);
-                            }
-                            break;
-                    }
-                } else {
-                    //zero 也不是*
-                    if (aa.length() == 2) {
-                        if (zeroA == '.') {
-                            result = isMatch(aa.substring(1), a.substring(1));
-                        } else {
-                            char zeroAA = aa.charAt(0);
-                            if (zeroA == zeroAA) {
-                                result = isMatch(aa.substring(1), a.substring(1));
-                            } else {
-                                result = false;
-                            }
-                        }
-                    } else {
-                        result = false;
-                    }
-                }
-                break;
-            default:
-                zeroA = a.charAt(0);
-                oneA = a.charAt(1);
 
                 if(aa.length() == 0){
                     if(oneA == '*'){
@@ -71,87 +26,65 @@ public class Solution {
                     }else{
                         result = false;
                     }
-                    break;
+                    break returnResult;
                 }
 
-
                 if (oneA == '*') {
+                    // 需要同时处理两个字符
                     String nextA = a.substring(2);
                     if (zeroA == '.') {
-                        //至少留一个字符
-                        for (int i = 0; i < aa.length(); i++) {
+                        for (int i = 0; i <= aa.length(); i++) {
                             String nextAA = aa.substring(i);
                             if(isMatch(nextAA, nextA)){
                                 result = true;
-                                break;
+                                break returnResult;
                             }
                         }
-                        result = false;
                     }else{
-                        if(isMatch(aa, nextA)){
-                            result = true;
-                            break;
-                        }
-                        if(aa.length() < 2){
-                            result = false;
-                            break;
-                        }
-                        for (int i = 0; i < aa.length(); i++) {
+                        for (int i = 0; i <= aa.length(); i++) {
+                            String nextAA = aa.substring(i);
+                            if(isMatch(nextAA, nextA)){
+                                result = true;
+                                break returnResult;
+                            }
+                            if(i == aa.length()){
+                                //没有字符可去了，不能再继续尝试了
+                                result = false;
+                                break returnResult;
+                            }
+                            //尝试再去掉一个字符
                             char current = aa.charAt(i);
                             if(current == zeroA){
-                                String nextAA = aa.substring(i);
-                                if(isMatch(nextAA, nextA)){
-                                    result = true;
-                                    break;
-                                }
-//                                else{
-//                                nextAA 还可以更短
-//                                    continue;
-//                                }
+                                //下次循环去掉一个字符
+                                continue;
                             }else{
                                 //第一个不等于zeroA的字符
-                                String nextAA = aa.substring(i);
-                                result = isMatch(nextAA, nextA);
-                                break;
+                                //不能再继续尝试了
+                                result = false;
+                                break returnResult;
                             }
                         }
-                        result = false;
-                        break;
                     }
-                    break;
+                    result = false;
+                    break returnResult;
                 } else {
-                    if (zeroA == '.') {
-//                        if(aa.length() < 2){
-//                            result = false;
-//                        }
+                    //只需要处理一个字符
+                    char zeroAA = aa.charAt(0);
+                    if (zeroA == '.' || zeroA == zeroAA) {
                         result = isMatch(aa.substring(1), a.substring(1));
                     } else {
-                        char zeroAA = aa.charAt(0);
-                        if (zeroA == zeroAA) {
-                            result = isMatch(aa.substring(1), a.substring(1));
-                        } else {
-                            result = false;
-                        }
+                        result = false;
                     }
-                    break;
+                    break returnResult;
                 }
         }
-        if(aa.length() <=3 && a.length() <= 4){
-            System.out.println("aa:" + aa + ",a:" + a + ",result:" + result  );
-        }
+        
+
+//        if(aa.length() <=3 && a.length() <= 4){
+//            System.out.println("aa:" + aa + ",a:" + a + ",result:" + result  );
+//        }
 
         return result;
 
-    }
-
-    private boolean isOneChar(String s, char c){
-        for (int i = 0; i < s.length(); i++) {
-            if(c == s.charAt(i)){
-
-            }else{
-                return false;
-            }
-        }
-        return true;
     }
 }
