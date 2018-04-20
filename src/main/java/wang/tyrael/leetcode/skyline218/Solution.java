26e7515920af5b59ff0d1711af6446f8a38fe909
王超 <chao198802@163.com>
2018-04-20 08:06:42 +0800

    skyline

--- src/main/java/wang/tyrael/leetcode/skyline218/Solution.java
+++ src/main/java/wang/tyrael/leetcode/skyline218/Solution.java
@@ -7,22 +7,28 @@ public class Solution {
         static final int TYPE_START = 0;
         static final int TYPE_END = 1;
 
-        int coordinate;
-        int other;
+        int start;
+        int end;
         int type;
         int height;
 
-        public Event(int coordinate, int type, int height) {
-            this.coordinate = coordinate;
+        public Event(int start, int end, int type, int height) {
+            this.start = start;
+            this.end = end;
             this.type = type;
             this.height = height;
         }
 
-        public Event(int coordinate, int other, int type, int height) {
-            this.coordinate = coordinate;
-            this.other = other;
-            this.type = type;
-            this.height = height;
+        /**
+         * 事件发生的坐标
+         * @return
+         */
+        public int getHappenPoint(){
+            if (type == Event.TYPE_START){
+                return start;
+            }else{
+                return end;
+            }
         }
     }
 
@@ -42,11 +48,29 @@ public class Solution {
     PriorityQueue<Event> queue = new PriorityQueue<>(new Comparator<Event>() {
         @Override
         public int compare(Event o1, Event o2) {
-            int dif = o1.coordinate - o2.coordinate;
+            int dif = o1.getHappenPoint() - o2.getHappenPoint();
             if (dif > 0) {
                 return 1;
             } else if (dif == 0) {
-                return 0;
+                //按常规习惯，end排前面
+                if(o1.type == o2.type){
+                    //高度按从高到低排列
+                    int heightDif = o1.height - o2.height;
+                    if(heightDif < 0){
+                        return 1;
+                    }else if(heightDif > 0){
+                        return -1;
+                    }else{
+                        return 0;
+                    }
+                }else{
+                    if(o1.type == Event.TYPE_END){
+                        return -1;
+                    }
+                    if(o2.type == Event.TYPE_END){
+                        return -1;
+                    }
+                }
             } else {
                 return -1;
             }
@@ -62,24 +86,36 @@ public class Solution {
             queue.add(new Event(current[1], Event.TYPE_END, current[2]));
         }
         int heightCurrent = 0;
+        Event lastEvent = null;
         while (!queue.isEmpty()) {
             Event event = queue.poll();
             //System.out.println(event.coordinate);
             switch (event.type) {
                 case Event.TYPE_START:
                     if (event.height > heightCurrent){
+                        if(lastEvent.){
+
+                        }
+
                         heightCurrent = event.height;
                         result.add(new int[]{event.coordinate, heightCurrent});
                     }
-                    int end = heightToEnd.getOrDefault(event.height, 0);
-                    if (end < event.coordinate){
+                    int end = heightToEnd.getOrDefault(event.height, -1);
+                    if (end < event.other){
                         //续命
-                        heightToEnd.put(event.height, event.coordinate);
+                        heightToEnd.put(event.height, event.other);
                     }
                     break;
                 case Event.TYPE_END:
-                    int end2 = heightToEnd.get(event.height);
-                    if(end2 == event.coordinate){
+                    //把同一个事件点的事件都取出来。
+
+
+                    int end2 = heightToEnd.getOrDefault(event.height, -1);
+                    if(end2 == -1){
+                        //这里要注意重复的点，对应的height上次已经删除了
+                        //heightToEnd不用处理
+                        //result不用处理
+                    } else if(end2 == event.coordinate){
                         //真的终结了
                         heightToEnd.remove(event.height);
                         if(event.height == heightCurrent){
@@ -101,6 +137,7 @@ public class Solution {
                     }
                     break;
             }
+            lastEvent = event;
         }
 
         return result;
