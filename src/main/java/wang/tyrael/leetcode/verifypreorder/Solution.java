340d4effb6949ff08369f277557ca5f800d71a23
王超 <343331640@qq.com>
2018-04-23 19:27:27 +0800

    hah

---
+++ src/main/java/wang/tyrael/leetcode/verifypreorder/Solution.java
@@ -0,0 +1,8 @@
+package wang.tyrael.leetcode.verifypreorder;
+
+/**
+ * @author: wangchao
+ * 2018/4/23 0023
+ */
+public class Solution {
+}
---
+++ src/test/java/wang.tyrael/leetcode/VerifyPreorder331.java
@@ -0,0 +1,8 @@
+package wang.tyrael.leetcode;
+
+/**
+ * @author: wangchao
+ * 2018/4/23 0023
+ */
+public class VerifyPreorder331 {
+}
