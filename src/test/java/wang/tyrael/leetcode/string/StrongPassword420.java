0dabf319eeadb9e4368583272c857bf671c8d637
Tyrael <343331640@qq.com>
2018-06-26 08:55:27 +0800

    1

--- src/test/java/wang/tyrael/leetcode/string/StrongPassword420.java
+++ src/test/java/wang/tyrael/leetcode/string/StrongPassword420.java
@@ -9,4 +9,20 @@ public class StrongPassword420 {
     public void test(){
         new TestTask(new Solution(), "strongPasswordChecker", new Object[]{"aa"}, 4).run();
     }
+
+    @Test
+    public void test1(){
+        new TestTask(new Solution(), "strongPasswordChecker", new Object[]{"aaa111"}, 2).run();
+    }
+
+    @Test
+    public void test2(){
+        new TestTask(new Solution(), "strongPasswordChecker", new Object[]{""}, 6).run();
+    }
+
+    @Test
+    public void test3(){
+        new TestTask(new Solution(), "strongPasswordChecker", new Object[]{"1010101010aaaB10101010"}, 2).run();
+    }
+
 }
